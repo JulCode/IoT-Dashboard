3db2ee914711c19ff93636a421f8ec30847ce6df
Julian Vivas <87610014+JulCode@users.noreply.github.com>
2022-06-10 07:27:09 -0500

    sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,30 +3,35 @@
     <notifications></notifications>
 
     <side-bar :background-color="sidebarBackground" short-title="Dash" title="IoT Dashboard">
-      <template slot-scope="props" slot="links">
-
+      <template slotScope="props" slot="links">
         <sidebar-item :link="{
           name: 'Dashboard',
           icon: 'tim-icons icon-chart-pie-36',
-          path: '/'
+          path: '/dashboard'
+        }">
+        </sidebar-item>
+        <sidebar-item :link="{
+          name: 'Devices',
+          icon: 'tim-icons icon-chart-pie-36',
+          path: '/devices'
+        }">
+        </sidebar-item>
+        <sidebar-item :link="{
+          name: 'Alarms',
+          icon: 'tim-icons icon-chart-pie-36',
+          path: '/alarms'
+        }">
+        </sidebar-item>
+        <sidebar-item :link="{
+          name: 'Templates',
+          icon: 'tim-icons icon-chart-pie-36',
+          path: '/templates'
         }">
         </sidebar-item>
 
-
-
-
-
-
-        <li class="active-pro">
-          <a href="https://www.creative-tim.com/product/nuxt-black-dashboard-pro" target="_blank">
-            <i class="tim-icons icon-spaceship"></i>
-            <p>Upgrade to PRO</p>
-          </a>
-        </li>
       </template>
     </side-bar>
-    <!--Share plugin (for demo purposes). You can remove it if don't plan on using it-->
-    <sidebar-share :background-color.sync="sidebarBackground"> </sidebar-share>
+
     <div class="main-panel" :data="sidebarBackground">
       <dashboard-navbar></dashboard-navbar>
       <router-view name="header"></router-view>
@@ -43,9 +48,9 @@
 </template>
 <script>
 /* eslint-disable no-new */
-import PerfectScrollbar from 'perfect-scrollbar';
-import 'perfect-scrollbar/css/perfect-scrollbar.css';
-import SidebarShare from '@/components/Layout/SidebarSharePlugin';
+import PerfectScrollbar from "perfect-scrollbar";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+import SidebarShare from "@/components/Layout/SidebarSharePlugin";
 function hasElement(className) {
   return document.getElementsByClassName(className).length > 0;
 }
@@ -61,10 +66,10 @@ function initScrollbar(className) {
   }
 }
 
-import DashboardNavbar from '@/components/Layout/DashboardNavbar.vue';
-import ContentFooter from '@/components/Layout/ContentFooter.vue';
-import DashboardContent from '@/components/Layout/Content.vue';
-import { SlideYDownTransition, ZoomCenterTransition } from 'vue2-transitions';
+import DashboardNavbar from "@/components/Layout/DashboardNavbar.vue";
+import ContentFooter from "@/components/Layout/ContentFooter.vue";
+import DashboardContent from "@/components/Layout/Content.vue";
+import { SlideYDownTransition, ZoomCenterTransition } from "vue2-transitions";
 
 export default {
   components: {
@@ -77,12 +82,12 @@ export default {
   },
   data() {
     return {
-      sidebarBackground: 'vue' //vue|blue|orange|green|red|primary
+      sidebarBackground: "vue" //vue|blue|orange|green|red|primary
     };
   },
   computed: {
     isFullScreenRoute() {
-      return this.$route.path === '/maps/full-screen'
+      return this.$route.path === "/maps/full-screen";
     }
   },
   methods: {
@@ -93,16 +98,16 @@ export default {
     },
     initScrollbar() {
       let docClasses = document.body.classList;
-      let isWindows = navigator.platform.startsWith('Win');
+      let isWindows = navigator.platform.startsWith("Win");
       if (isWindows) {
         // if we are on windows OS we activate the perfectScrollbar function
-        initScrollbar('sidebar');
-        initScrollbar('main-panel');
-        initScrollbar('sidebar-wrapper');
+        initScrollbar("sidebar");
+        initScrollbar("main-panel");
+        initScrollbar("sidebar-wrapper");
 
-        docClasses.add('perfect-scrollbar-on');
+        docClasses.add("perfect-scrollbar-on");
       } else {
-        docClasses.add('perfect-scrollbar-off');
+        docClasses.add("perfect-scrollbar-off");
       }
     }
   },
---
+++ pages/devices.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>Devicess</h2>
+  </div>
+</template>
