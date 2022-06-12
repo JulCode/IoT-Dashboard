991bb4c488e318899fcd477e11d84b776fa779be
Julian Vivas <87610014+JulCode@users.noreply.github.com>
2022-06-12 08:16:54 -0500

    register Endpoint

--- api/routes/devices.js
+++ api/routes/devices.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/device", (req, res) => {}); // get all devices
-router.post("/device", (req, res) => {}); // create new device
-router.delete("/device", (req, res) => {}); //delete device
-router.put("/device", (req, res) => {}); //update device
+//get all devices
+router.get("/device", (req, res) => {
+  console.log(req.query);
+  var toReturn = {
+    status: "ok",
+    data: "a b c d e f"
+  };
+  res.json(toReturn);
+});
+//create new device
+router.post("/device", (req, res) => {});
+//delete device
+router.delete("/device", (req, res) => {});
+//update device
+router.put("/device", (req, res) => {});
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,14 +1,46 @@
 const express = require("express");
 const router = express.Router();
 //const jws = require("jsonwebtoken");
-//const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 //models import
 import User from "../models/user";
 
+//POST -> req.body
+//GET -> req.query
+
 //AUTH
-router.post("/register", (req, res) => {}); // create new user
-router.post("/login", (req, res) => {}); // login user
+// create new user
+router.post("/register", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const encryptedPassword = bcrypt.hashSync(password, 10);
+
+    const newUser = {
+      name: name,
+      email: email,
+      password: encryptedPassword
+    };
+    const user = await User.create(newUser);
+    const toSend = {
+      status: "ok"
+    };
+    res.json(toSend);
+  } catch (error) {
+    console.log("Error - register endpoint ", error);
+
+    const toSend = {
+      status: "error",
+      message: error.message
+    };
+    res.status(500).json(toSend);
+  }
+});
+
+// login user
+router.post("/login", (req, res) => {});
 
 router.get("/new-user", async (req, res) => {
   try {
