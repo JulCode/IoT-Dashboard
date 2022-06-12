9d1c051d28b08f72c0e65d73858d6be6d3f0c287
Julian Vivas <87610014+JulCode@users.noreply.github.com>
2022-06-12 13:55:23 -0500

    add device Endpoint

--- api/routes/devices.js
+++ api/routes/devices.js
@@ -3,19 +3,44 @@ const router = express.Router();
 
 const { checkAuth } = require("../middlewares/authentication");
 
-//MODELS
+/*-----------------MODELS--------------------*/
 import Device from "../models/device";
-//API
+/*-----------------API--------------------*/
 
 //get all devices
-router.get("/device", checkAuth, (req, res) => {});
+router.get("/device", checkAuth, async (req, res) => {});
 //create new device
-router.post("/device", (req, res) => {});
+router.post("/device", checkAuth, async (req, res) => {
+  try {
+    const userId = req.userData._id;
+    var newDevice = req.body.newDevice;
+
+    console.log(newDevice);
+
+    newDevice.userId = userId;
+    newDevice.createdTime = Date.now();
+
+    const device = await Device.create(newDevice);
+
+    const toSend = {
+      status: "success"
+    };
+    res.json(toSend);
+  } catch (error) {
+    console.log("Error CREATING NEW DEVICE: " + error);
+
+    const toSend = {
+      status: "error",
+      message: error.message
+    };
+    return res.status(500).json(toSend);
+  }
+});
 //delete device
 router.delete("/device", (req, res) => {});
 //update device
 router.put("/device", (req, res) => {});
 
-//Function
+/*-----------------fucntion--------------------*/
 
 module.exports = router;
