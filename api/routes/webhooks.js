1380d3e58e44d18a9e444863940678755c911b61
Julian Vivas <87610014+JulCode@users.noreply.github.com>
2022-06-14 17:51:05 -0500

    creamos recursos EMQX desde node

--- api/routes/webhooks.js
+++ api/routes/webhooks.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const axios = require("axios");
 const router = express.Router();
+const axios = require("axios");
 const colors = require("colors");
 
 router.post("/saver-webhook", async (req, res) => {
   const data = req.body;
-  console.log(data);
+  console.log(data.m);
+  res.json("{}");
 });
 
 module.exports = router;
