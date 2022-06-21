312fab96a0fd53aa3ed3a67b792c4201cf2bef7a
Julian Vivas <87610014+JulCode@users.noreply.github.com>
2022-06-21 13:08:12 -0500

    cliente MQTT en el back

--- api/routes/webhooks.js
+++ api/routes/webhooks.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const colors = require("colors");
+var mqtt = require("mqtt");
 
 import Data from "../models/data.js";
 import Device from "../models/device.js";
 import Notification from "../models/notifications.js";
 import AlarmRule from "../models/emqx_alarm_rule.js";
+
+var client;
 
 //SAVER WEBHOOK
 router.post("/saver-webhook", async (req, res) => {
@@ -65,12 +68,14 @@ router.post("/alarm-webhook", async (req, res) => {
     if (lastNotif == 0) {
       console.log("FIRST TIME ALARM");
       saveNotifToMongo(incomingAlarm);
+      sendMqttNotif(incomingAlarm);
     } else {
       const lastNotifToNowMins = (Date.now() - lastNotif[0].time) / 1000 / 60;
 
       if (lastNotifToNowMins > incomingAlarm.triggerTime) {
         console.log("TRIGGERED");
         saveNotifToMongo(incomingAlarm);
+        sendMqttNotif(incomingAlarm);
       }
     }
   } catch (error) {
@@ -78,6 +83,52 @@ router.post("/alarm-webhook", async (req, res) => {
     res.sendStatus(200);
   }
 });
+
+function startMqttClient() {
+  const options = {
+    port: 1883,
+    host: "localhost",
+    clientId:
+      "webhook_superuser" + Math.round(Math.random() * (0 - 10000) * -1),
+    username: "superuser",
+    password: "superuser",
+    keepalive: 60,
+    reconnectPeriod: 5000,
+    protocolId: "MQIsdp",
+    protocolVersion: 3,
+    clean: true,
+    encoding: "utf8"
+  };
+
+  client = mqtt.connect("mqtt://" + "localhost", options);
+
+  client.on("connect", function() {
+    console.log("MQTT CONNECTION -> SUCCESS;".green);
+    console.log("\n");
+  });
+
+  client.on("reconnect", error => {
+    console.log("RECONNECTING MQTT");
+    console.log(error);
+  });
+
+  client.on("error", error => {
+    console.log("MQTT CONNECIONT FAIL -> ");
+    console.log(error);
+  });
+}
+
+function sendMqttNotif(notif) {
+  const topic = notif.userId + "/dummy-did/dummy-var/notif";
+  const msg =
+    "The rule: when the " +
+    notif.variableFullName +
+    " is " +
+    notif.condition +
+    " than " +
+    notif.value;
+  client.publish(topic, msg);
+}
 
 function saveNotifToMongo(incomingAlarm) {
   try {
@@ -100,5 +151,9 @@ async function updateAlarmCounter(emqxRuleId) {
     console.log(error);
   }
 }
+
+setTimeout(() => {
+  startMqttClient();
+}, 3000);
 
 module.exports = router;
